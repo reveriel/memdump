492b5314e4422538d3e6b438a4bd95f6d24d9232
Reveriel <reveriel@hotmail.com>
2019-01-04 10:29:27 +0800

    build for one target

--- jni/Android.mk
+++ jni/Android.mk
@@ -9,13 +9,6 @@ LOCAL_MODULE := memdump
 LOCAL_SRC_FILES := main.c
 LOCAL_CPPFLAGS := -std=gnu++0x -Wall -fPIE         # whatever g++ flags you like
 # LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog -fPIE -pie   # whatever ld flags you like
-APP_ABI := arm64-v8a
-TARGET_ARCH := arm64-v8a
-TARGET_ARCH_ABI := arm64-v8a
-TARGET_ABI := android-26-arm64-v8a
-TARGET_PLATFORM := android-26
-APP_PLATFORM := android-26
-# TAT, doesn't work, how to specify one target arch
 
 include $(BUILD_EXECUTABLE)    # <-- Use this to build an executable.
 
@@ -27,11 +20,5 @@ LOCAL_MODULE := testnapp
 LOCAL_SRC_FILES := testnapp.c
 LOCAL_CPPFLAGS := -std=gnu++0x -Wall -fPIE         # whatever g++ flags you like
 # LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog -fPIE -pie   # whatever ld flags you like
-APP_ABI := arm64-v8a
-TARGET_ARCH := arm64-v8a
-TARGET_ARCH_ABI := arm64-v8a
-TARGET_ABI := android-26-arm64-v8a
-TARGET_PLATFORM := android-26
-APP_PLATFORM := android-26
 
 include $(BUILD_EXECUTABLE)    # <-- Use this to build an executable.
---
+++ jni/Application.mk
@@ -0,0 +1,11 @@
+# ?? put here, not Android.mk!
+# see
+# https://software.intel.com/en-us/articles/building-an-android-command-line-application-using-the-ndk-build-tools
+
+
+APP_ABI := arm64-v8a
+TARGET_ARCH := arm64-v8a
+TARGET_ARCH_ABI := arm64-v8a
+TARGET_ABI := android-26-arm64-v8a
+TARGET_PLATFORM := android-26
+APP_PLATFORM := android-26
