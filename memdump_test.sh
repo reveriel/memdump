c6bd31c60130cbd2b5410c1ce8af6795c9830434
Reveriel <reveriel@hotmail.com>
2019-01-17 16:31:50 +0800

    head -n1 in cace of fork

--- memdump_test.sh
+++ memdump_test.sh
@@ -4,7 +4,7 @@ ndk-build
 ./push.sh
 adb shell killall testnapp
 adb shell /data/local/tmp/testnapp &
-pid=$(adb shell ps -e | grep testnapp | sed 's/[ ][ ]*/ /g' | cut -d' ' -f2)
+pid=$(adb shell ps -e | grep testnapp | sed 's/[ ][ ]*/ /g' | cut -d' ' -f2 | head -n1)
 echo "pid is " $pid
 adb shell /data/local/tmp/memdump $pid
 adb shell killall testnapp
